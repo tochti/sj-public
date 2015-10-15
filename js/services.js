e44b037291af2ab65316081468cb1360e168eeee
Tim Tochtermann <tim.tochtermann@gmail.com>
2015-10-15 14:34:53 +0200

    start G, Series and Shelf Service

--- js/services.js
+++ js/services.js
@@ -4,7 +4,269 @@ var appServices = angular.module('AppServices', []);
 
 appServices.factory('User', [
   function () {
-    return {};
+    var user = {
+      _state: false,
+      _name: "",
+      _pass: "",
+      _id: "",
+
+      setup: function (spec) {
+        this._name = spec.name;
+        this._pass = spec.pass;
+      },
+
+      name: function () {
+        return this._name;
+      },
+
+      pass: function () {
+        return this._pass;
+      },
+
+      id: function () {
+        return this._id;
+      },
+
+      login: function (id) {
+        this._id = id;
+        this._state = true;
+      },
+
+      signedIn: function () {
+        return this._state;
+      }
+
+    }
+
+    return user;
   }
 ]);
 
+
+appServices.factory('Series', ['$http', '$q',
+  function ($http, $q) {
+    var isValidData = function (data) {
+      if (data === undefined) {
+        return false
+      }
+
+      if (data.Title === undefined ||
+          data.Image === undefined || 
+          data.Desc === undefined || 
+          data.Episodes === undefined ||
+          data.Portal === undefined) {
+        return false
+      }
+
+      return true
+    }
+    return function (data) {
+
+      if (!isValidData(data)) {
+        return {}
+      }
+
+      var series = {
+        _urlPrefix: '/Series',
+        _id: '',
+        _title: data.Title,
+        _image: data.Image,
+        _desc: data.Desc,
+        _episodes: data.Episodes,
+        _portal: data.Portal,
+
+        id: function () {
+          return this._id;
+        },
+
+        save: function () {
+          var that = this;
+          var d = $q.defer();
+
+          var success = function (resp) {
+            if (resp.data.Status === 'success') {
+              that._id = resp.data.Data.ID;
+              d.resolve(resp);
+            }
+
+            d.reject(resp);
+          }
+
+          var error = function (resp) {
+            d.reject(resp);
+          }
+
+          var url = this._urlPrefix;
+          var req = {
+            Data: {
+              Title: this._title,
+              Image: this._image,
+              Desc: this._desc,
+              Episodes: this._episodes,
+              Portal: this._portal,
+            },
+          }
+          $http.post(url, req).then(success, error);
+
+          return d.promise;
+        },
+
+        remove: function () {
+          var d = $q.defer();
+
+          var success = function (resp) {
+            if (resp.data.Status === 'success') {
+              d.resolve(resp);
+            }
+
+            d.reject(resp);
+          }
+
+          var error = function (resp) {
+            d.reject(resp);
+          }
+
+          var url = this._urlPrefix +'/'+ this.id();
+          $http.delete(url).then(success, error);
+
+          return d.promise;
+
+        },
+
+      };
+
+      return series;
+
+    }
+  }
+]);
+
+// Verwaltet eine Liste von Objekten
+appServices.factory('Shelf', [
+  function () {
+    return {
+      _objs: [],
+      // Fügt ein Objekt der Liste hinzu
+      // Das Objekt welches hinzugefügt wird muss folgende Funktionen besitzen
+      // id() string
+      // data() {}
+      //
+      // append(obj {}) bool
+      append: function (obj) {
+        if (obj.constructor === Array) {
+          return this.appendBatch(obj);
+        }
+
+        return this.appendOne(obj);
+
+      },
+      appendBatch: function (objs) {
+        var that = this;
+        var ok = true;
+
+        angular.forEach(objs, function (val, i) {
+          var r = that.appendOne(val);
+          if (!r) {
+            ok = false;
+            return;
+          }
+        });
+
+        return ok;
+      },
+      appendOne: function (obj) {
+        if (typeof obj.id !== 'function' || typeof obj.data !== 'function') {
+          return false
+        }
+
+        this._objs.push(obj);
+        return true
+      },
+      // Entfernt ein Objekt mit der id X aus der Liste
+      // 
+      // remove(id string) {...}
+      remove: function (id) {
+        var found = [];
+        var index = this.indexOfId(id);
+        if (index === -1) {
+          return {}
+        }
+
+        return this._objs.splice(index, 1)[0];
+      },
+      // Ersetzte ein Objekt durch ein geupdateds Objekt 
+      update: function (id, updatedObj) {
+        var i = this.indexOfId(id);
+        this._objs[i] = updatedObj;
+      },
+      // Findet die Position eines Objekts mit der id X
+      indexOfId: function (id) {
+        var found = -1;
+        angular.forEach(this._objs, function (val, index) {
+          if (id === val.id()) {
+            found = index;
+            return;
+          }
+        });
+
+        return found;
+      },
+      // Gibt das Objekt mit der id X zurück
+      read: function (id) {
+        var i = this.indexOfId(id);
+        if (i === -1) {
+          return {}
+        }
+
+        return this._objs[i];
+      },
+      // Gibt ein Array mit allen Objekten zurück
+      list: function () {
+        return this._objs;
+      },
+      // Findet ein Liste mit Objekten zurück im Feld X das Pattern Y haben.
+      find: function(field, pattern) {
+        var regx = new RegExp(pattern, 'i');
+        var found = [];
+
+        angular.forEach(this._objs, function (val, i) {
+          if (val.data()[field].match(regx)) {
+            found.push(val);
+          }
+        });
+
+        return found;
+      },
+    }
+  }
+]);
+
+appServices.factory('G', ['$http', '$q',
+  function ($http, $q) {
+    return {
+      // Expect user to have a id() string function
+      readSeriesOfUser: function (user) {
+        var d = $q.defer();
+
+        var url = '/SeriesOfUser/'+ user.id();
+        var success = function (resp) {
+          if (resp.data.Status !== 'success') {
+            d.reject(resp);
+            return
+          }
+
+          d.resolve(resp);
+
+        }
+        
+        var error = function (resp) {
+          d.reject(resp);
+        }
+
+        $http.get(url).then(success, error);
+
+        return d.promise;
+      },
+    }
+  }
+]);
--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -6,21 +6,20 @@ describe('Services', function() {
 
   describe('User', function () {
 
-    var $scope,
+    var $s,
       $rootScope,
       userName,
-      userPass,
-      user;
+      userPass;
 
     beforeEach(inject(function ($injector) {
       $rootScope = $injector.get('$rootScope')
-      $scope = $rootScope.$new(); 
-      user = $injector.get('User');
+      $s = $rootScope.$new(); 
+      $s.user = $injector.get('User');
 
       userName = "LoverXX";
       userPass = "1234";
 
-      user.setup({
+      $s.user.setup({
         name: userName,
         pass: userPass,
       })
@@ -28,19 +27,211 @@ describe('Services', function() {
 
     }));
 
-    describe('should have user method', function () {
-      expect(user.name()).toBe(userName)
+    it('should have user method', function () {
+      expect($s.user.name()).toBe(userName)
     });
 
-    describe('should have pass method', function () {
-      expect(user.pass()).toBe(userPass)
+    it('should have pass method', function () {
+      expect($s.user.pass()).toBe(userPass)
     });
 
-    describe('loggin user', function () {
-      expect(user.singedIn()).toBe(false);
-      user.login(true)
-      expect(user.singedIn()).toBe(true);
+    it('should have id method', function () {
+      $s.user.login('123');
+      expect($s.user.id()).toBe('123');
+    });
+
+    it('loggin user', function () {
+      expect($s.user.signedIn()).toBe(false);
+      $s.user.login('123');
+      expect($s.user.signedIn()).toBe(true);
     });
 
   });
+
+  describe('Series', function () {
+
+    var $rootScope,
+        $s,
+        $httpBackend,
+        seriesTitle,
+        seriesRescName,
+        seriesRescURL,
+        seriesID,
+        seriesData;
+
+    beforeEach(inject(function ($injector) {
+      $httpBackend = $injector.get('$httpBackend');
+      $rootScope = $injector.get('$rootScope');
+      $s = $rootScope.$new();
+      $s.Series = $injector.get('Series');
+      seriesTitle = 'Mr. Robot';
+      seriesRescName = 'imdb.com';
+      seriesRescURL = 'http://imdb.com/mr+robot';
+      seriesID = "123";
+      seriesData = {
+        Title: seriesTitle,
+        Image: {
+          Name: seriesRescName,
+          URL: seriesRescURL,
+        },
+        Desc: {
+          Name: seriesRescName,
+          URL: seriesRescURL,
+        },
+        Episodes: {
+          Name: seriesRescName,
+          URL: seriesRescURL,
+        },
+        Portal: {
+          Name: seriesRescName,
+          URL: seriesRescURL,
+        }
+      };
+
+    }));
+
+    it('crud series', function (done) {
+      $httpBackend
+        .expectPOST('/Series')
+        .respond({'Status': 'success', 'Data': {'ID': seriesID}});
+
+      var series = new $s.Series(seriesData);
+      series.save().then(
+        function (resp) {
+          expect(series.id()).toBe(seriesID)
+        }
+      );
+
+      $httpBackend.flush();
+      $s.$apply();
+
+      $httpBackend
+        .expectDELETE('/Series/'+ seriesID)
+        .respond({'Status': 'success', 'Data': {'ID': seriesID}});
+
+      series.remove().then(
+        function (resp) {
+          done();
+        }
+      );
+
+      $httpBackend.flush();
+      $s.$apply();
+
+    });
+
+  });
+
+  describe('Shelf', function () {
+
+    var $rootScope,
+      $s;
+
+    var O = function (d) {
+      var o = {
+        data: function () {
+          return d.data
+        },
+        id: function () {
+          return d.id
+        },
+      }
+
+      return o;
+    }
+      
+
+    beforeEach(inject(function($injector) {
+      $rootScope = $injector.get('$rootScope');
+      $s = $rootScope.$new();
+      $s.shelf = $injector.get('Shelf');
+    }));
+
+    it('crud objects', function() {
+      var obj1 = new O({
+        id: "1",
+        data: {
+          Name: "one",
+        },
+      });
+
+      var obj2 = new O({
+        id: "2",
+        data: {
+          Name: "two",
+        },
+      });
+      var objs = [obj1, obj2]
+
+      expect($s.shelf.append(objs)).toBe(true);
+      expect($s.shelf.list()).toEqual(objs);
+
+      var o = $s.shelf.remove(obj1.id());
+      expect(o).toEqual(obj1);
+      expect($s.shelf.list()).toEqual([obj2]);
+
+      o = $s.shelf.read(obj2.id());
+      expect(o).toEqual(obj2);
+
+      var updated = new O({
+        id: "2",
+        data: {
+          Name: "TWO",
+        },
+      });
+      $s.shelf.update(obj2.id(), updated);
+      o = $s.shelf.read(obj2.id());
+      expect(o.data()).toEqual(updated.data());
+
+      var r = $s.shelf.find('Name', 'T');
+      expect(r).toEqual([updated]);
+
+    });
+
+  });
+
+  describe('G', function () {
+    var $httpBackend,
+      $s,
+      $rootScope,
+      userID;
+
+    describe('readSeriesOfUser', function () {
+
+      beforeEach(inject(function ($injector) {
+        $httpBackend = $injector.get('$httpBackend');
+        $rootScope = $injector.get('$rootScope');
+        $s = $rootScope.$new();
+        $s.g = $injector.get('G');
+        $s.series = $injector.get('Series');
+        userID = '1';
+        $s.user = $injector.get('User');
+        $s.user.setup({
+          name: 'why',
+          pass: '123',
+        });
+        $s.user.login(userID);
+
+      }));
+
+      it('should return all series of a user', function (done) {
+        var expectResp = {'Status': 'success', 'Data': [{'Title': 'Mr. Robot'}]};
+        $httpBackend
+          .expectGET('/SeriesOfUser/'+ userID)
+          .respond(expectResp);
+
+        $s.g.readSeriesOfUser($s.user).then(function (resp) {
+          expect(resp.data).toEqual(expectResp);
+          done();
+        });
+
+        $httpBackend.flush();
+        $s.$apply();
+
+      });
+
+    });
+
+  });
+
 });
